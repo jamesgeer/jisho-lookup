dfbc0c9081a8520b7513ecf11021e73ff9c99085
James <jgeer@liquid-computing.co.uk>
2021-01-30 21:19:02 +0000

    iframe blocked, switching to new tab instead

--- lookup.js
+++ lookup.js
@@ -8,9 +8,37 @@ browser.contextMenus.create({
 browser.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "lookup-jisho") {
 
+        // const selectedText = select.selectionText;
+
+        console.log("test");
+
+        //const selection = window.getSelection();
+
         const selectedText = info.selectionText;
 
-        window.open("https://jisho.org/search/" + selectedText, "", "width=200,height=100");
+        // const range = selection.getRangeAt(0);
+        // const rect = range.getBoundingClientRect();
+        // const top = Math.round(rect.top);
+        // const left = Math.round(rect.left);
+
+        console.log("hello");
+
+        // console.log("top:" + top);
+        // console.log("left:" + left);
+
+        const iframe = document.createElement("iframe");
+        iframe.sandbox = 'allow-forms allow-scripts allow-same-origin';
+        
+        iframe.src = "https://jisho.org/search/" + selectedText;
+        // iframe.style.width = '1100px';
+        // iframe.style.height = '800px';
+        iframe.style.position = 'absolute';
+        //iframe.style.top = top + 'px';
+        //iframe.style.left = left + 'px';
+
+        console.log(iframe);
+
+        document.body.appendChild(iframe)
 
     }
 });
--- test/lookup.js
+++ test/lookup.js
@@ -2,7 +2,7 @@ document.addEventListener('mouseup', event => {
     if(window.getSelection().toString().length){
 
         const selection = window.getSelection();
-        const selectText = selection.toString();
+        const selectedText = selection.toString();
         const range = selection.getRangeAt(0);
         const rect = range.getBoundingClientRect();
         const top = Math.round(rect.top);
@@ -11,15 +11,15 @@ document.addEventListener('mouseup', event => {
         console.log("top:" + top);
         console.log("left:" + left);
 
-        // const iframe = document.createElement("iframe");
-        // iframe.src = "https://jisho.org/search/" + exactText;
-        // iframe.style.width = '1100px';
-        // iframe.style.height = '800px';
-        // iframe.style.position = 'absolute';
-        // iframe.style.left = window.pageX + 'px';
-        // iframe.style.top = window.pageY + 'px';
+        const iframe = document.createElement("iframe");
+        iframe.src = "https://jisho.org/search/" + selectedText;
+        iframe.style.width = '1100px';
+        iframe.style.height = '800px';
+        iframe.style.position = 'absolute';
+        iframe.style.top = top + 'px';
+        iframe.style.left = left + 'px';
 
-        // document.body.appendChild(iframe)
+        document.body.appendChild(iframe)
 
         //window.open("https://jisho.org/search/" + exactText, "", "width=800,height=800");
     }
