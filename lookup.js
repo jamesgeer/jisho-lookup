c6bdd28803208b5c1f16bd175a024c69e142072e
James <jgeer@liquid-computing.co.uk>
2021-02-06 00:10:27 +0000

    live

--- lookup.js
+++ lookup.js
@@ -1,22 +1,16 @@
 browser.contextMenus.create({
     id: "lookup-jisho",
     title: "Jisho Lookup",
-    contexts: ["selection"],
-    icons: {
-        "32": "jisho.png"
-    }
+    contexts: ["selection"]
 });
 
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "lookup-jisho") {
-
         const selectedText = info.selectionText;
-
         const creating = browser.tabs.create({
             url:"https://jisho.org/search/" + selectedText
           });
           creating.then(onCreated, onError);
-
     }
 }); 
